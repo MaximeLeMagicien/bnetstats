ccf9e77e2969d6532ee3e596f79af4bf1f90f392
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-22 23:29:15 +0000

    fixLoginIssues

--- pages/Login.py
+++ pages/Login.py
@@ -4,14 +4,14 @@ import webbrowser, requests
 
 
 try:
-    exists = requests.get(params.initialAppURL+f"getExistingCode?state={params.state}")
+    exists = requests.get(params.initialAppURL+f"api/getExistingCode?state={params.state}")
     st.write(exists.text)
     content = exists.json()
     if not content["exists"] :
-        webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}")
+        webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}")
     else:
         st.write(content["token"])
 except requests.exceptions.ConnectionError :
     pass
 
-webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}")
+webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}api/code&state={params.state}")
