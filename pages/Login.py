eb53a71b570abd402e9c7dad4f157735587628a2
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-22 23:53:24 +0000

    testStJavascript

--- pages/Login.py
+++ pages/Login.py
@@ -1,18 +1,20 @@
 import streamlit as st
 from Hello import params
 import webbrowser, requests
+from streamlit_javascript import st_javascript
 
 
 try:
     url = params.initialAppURL+f"api/getExistingCode?state={params.state}"
     exists = requests.get(url)
-    st.write(exists.json())
     content = exists.json()
     if content["exists"] == False:
-        webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}", 1)
+        url = f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}"
+        js = f'window.open("{url}", "_blank").then(r => window.parent.location.href);'
+        st_javascript(js)
     else:
         st.write(content["token"])
 except requests.exceptions.ConnectionError :
-    pass
-
-webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}api/code&state={params.state}")
+    url = f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}"
+    js = f'window.open("{url}", "_blank").then(r => window.parent.location.href);'
+    st_javascript(js)
