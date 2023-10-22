52a08e218e0cf6b31a36363bb8dbea0988e81ef1
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-22 23:35:33 +0000

    testfix

--- pages/Login.py
+++ pages/Login.py
@@ -6,7 +6,7 @@ import webbrowser, requests
 try:
     url = params.initialAppURL+f"api/getExistingCode?state={params.state}"
     exists = requests.get(url)
-    st.write(exists.text)
+    st.write(exists.json())
     content = exists.json()
     if content["exists"] == False:
         webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}")
