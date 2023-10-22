ded4c0c8ac1f78ff70f19215be02a9e31720d169
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-22 23:36:31 +0000

    newFix2

--- pages/Login.py
+++ pages/Login.py
@@ -9,7 +9,7 @@ try:
     st.write(exists.json())
     content = exists.json()
     if content["exists"] == False:
-        webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}")
+        webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=api/code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}", 1)
     else:
         st.write(content["token"])
 except requests.exceptions.ConnectionError :
