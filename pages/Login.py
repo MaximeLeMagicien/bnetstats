717f4bed329a37481d209ccc57173dbdde18d869
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-22 23:25:32 +0000

    fix

--- pages/Login.py
+++ pages/Login.py
@@ -5,10 +5,12 @@ import webbrowser, requests
 
 try:
     exists = requests.get(params.initialAppURL+f"getExistingCode?state={params.state}")
-    if not exists.json()["exists"] :
+    print(exists.text)
+    content = exists.json()
+    if not content["exists"] :
         webbrowser.open(f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri={params.initialAppURL}code&state={params.state}")
     else:
-        st.write(exists.json()["token"])
+        st.write(content["token"])
 except requests.exceptions.ConnectionError :
     pass
 
