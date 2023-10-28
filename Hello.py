8f3e7845dfb242c6636902076f8b05f0d82af6ac
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-28 05:02:15 +0000

    newTest

--- Hello.py
+++ Hello.py
@@ -36,7 +36,7 @@ def run():
       params.token = jwt.decode(params.sessionKey, key=KEY, algorithms='HS256')["initialTime"]
     except KeyError:
       pass
-    st.write(params.token)
+    st.write(f"token : {params.token}")
     redirect = params.initialAppURL+"api/code"
     params.state = jwt.encode({"initialAppURL" : redirect, "initalTime" : str(params.token)}, key=KEY, algorithm='HS256')
     st.sidebar.success("Select a demo above.")
