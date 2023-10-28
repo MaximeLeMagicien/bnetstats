9c760bda922909e7c1fb39118bbd0750d9726a1e
Liche911 <67310707+MaximeLeMagicien@users.noreply.github.com>
2023-10-28 04:58:28 +0000

    testFix3

--- pages/Login.py
+++ pages/Login.py
@@ -9,12 +9,12 @@ try:
     exists = requests.get(url)
     content = exists.json()
     if content["exists"] == False:
-        url = f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri=https://short.universjeux.info/bnetStats/&state={params.state}"
+        url = f'https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri=https://short.universjeux.info/bnetStats/&state="{params.state}"'
         js = f'window.open("{url}", "_blank").then(r => window.parent.location.href);'
         st_javascript(js)
     else:
         st.write(content["token"])
 except requests.exceptions.ConnectionError :
-    url = f"https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri=https://short.universjeux.info/bnetStats/&state={params.state}"
+    url = f'https://oauth.battle.net/oauth/authorize?response_type=code&client_id=1bc88a090bc14ad48d20b0e9e7d1e917&redirect_uri=https://short.universjeux.info/bnetStats/&state="{params.state}"'
     js = f'window.open("{url}", "_blank").then(r => window.parent.location.href);'
     st_javascript(js)
